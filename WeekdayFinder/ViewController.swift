0eb9158abb46d06e2a33a548d1aa17d22e80c13f
Kseniia Prytkova <prytkovakseniia@gmail.com>
2020-10-01 18:01:25 +0300

    add interface

--- WeekdayFinder/ViewController.swift
+++ WeekdayFinder/ViewController.swift
@@ -5,16 +5,17 @@
 //  Created by admin on 10/1/20.
 //  Copyright © 2020 Kseniia Prytkova. All rights reserved.
 //
-
+//Users/admin/swiftBook/WeekdayFinder/WeekdayFinder/ViewController.swift
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
     }
-
-
 }
 
