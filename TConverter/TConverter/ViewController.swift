95eca157669e15f8219129d443ddb60b5a1157e2
Kseniia Prytkova <prytkovakseniia@gmail.com>
2020-10-07 16:10:03 +0300

    add UI elements for T-Converter

---
+++ TConverter/TConverter/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Kseniia Prytkova on 10/7/20.
+//  Copyright © 2020 Kseniia Prytkova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
