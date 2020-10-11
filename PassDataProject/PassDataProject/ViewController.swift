b69ca0c46cbc416557d4caa3d69f2868c7c05698
Kseniia Prytkova <prytkovakseniia@gmail.com>
2020-10-11 17:47:27 +0300

    add first screen

---
+++ PassDataProject/PassDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Kseniia Prytkova on 10/11/20.
+//  Copyright © 2020 Kseniia Prytkova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
