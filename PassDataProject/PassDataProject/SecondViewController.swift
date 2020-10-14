ea8d7fd20e03b0ff8b088be544b3ec642c28703f
Kseniia Prytkova <prytkovakseniia@gmail.com>
2020-10-14 16:16:33 +0300

    add constraints, secons UIView, connections

---
+++ PassDataProject/PassDataProject/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Kseniia Prytkova on 10/14/20.
+//  Copyright © 2020 Kseniia Prytkova. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBAction func goBackTapped(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
--- PassDataProject/PassDataProject/ViewController.swift
+++ PassDataProject/PassDataProject/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
